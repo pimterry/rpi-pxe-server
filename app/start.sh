74c6450899f9df76115e396b9ff2a08d91180be1
Tim Perry <pimterry@gmail.com>
2017-02-11 12:57:45 +0100

    Host DHCP with dnsmasq

--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 printf "Starting up"
+
+printf "Setting ethernet ip to 192.168.0.1"
+ifconfig eth0 192.168.0.1 netmask 255.255.255.0 up
+
+printf "\n ** Following journalctl logs: **\n\n"
+journalctl -f
