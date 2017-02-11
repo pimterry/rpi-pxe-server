d867588f0f15a1bde1865633ed3d15c1b838bcf4
Tim Perry <pimterry@gmail.com>
2017-02-11 16:52:09 +0100

    Add samba

--- app/start.sh
+++ app/start.sh
@@ -5,5 +5,9 @@ printf "Starting up"
 printf "Setting ethernet ip to 192.168.0.1"
 ifconfig eth0 192.168.0.1 netmask 255.255.255.0 up
 
+printf "Setting up tftp share at /data/tftp and samba at /data/public"
+mkdir /data/tftp
+mkdir /data/public
+
 printf "\n ** Following journalctl logs: **\n\n"
 journalctl -f
