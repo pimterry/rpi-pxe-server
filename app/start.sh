c51808fe422292e08ecafe1e34d07aa1f46a0cd7
Tim Perry <pimterry@gmail.com>
2017-02-12 13:16:20 +0100

    Tidy up start script a little

--- app/start.sh
+++ app/start.sh
@@ -2,12 +2,12 @@
 
 printf "\n ** Starting up **\n\n"
 
-printf "Setting ethernet ip to 192.168.0.1"
+printf "Setting ethernet ip to 192.168.0.1\n"
 ifconfig eth0 192.168.0.1 netmask 255.255.255.0 up
 
-printf "Setting up tftp share at /data/tftp and samba at /data/public"
-mkdir /data/tftp
-mkdir /data/public
+printf "Setting up tftp share at /data/tftp and samba at /data/public\n"
+mkdir -p /data/tftp
+mkdir -p /data/public
 
 printf "\n ** Following journalctl logs: **\n\n"
 journalctl -f
